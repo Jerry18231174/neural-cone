06645e9685059e0e69897d13cb874d5be0fca86d
jerry_WD1T <1425830273@qq.com>
2025-04-14 14:03:38 +0800

    Reduce multi-card step

--- scripts/train.sh
+++ scripts/train.sh
@@ -10,7 +10,9 @@ NUM_GPUS=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
 echo "[train.sh] Using CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 echo "[train.sh] Detected $NUM_GPUS GPU(s)"
 
-source scripts/activate_mitsuba.sh
+# Get directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $DIR/active_mitsuba.sh
 
 # Start training
 torchrun --nproc_per_node=$NUM_GPUS train.py "$@"
